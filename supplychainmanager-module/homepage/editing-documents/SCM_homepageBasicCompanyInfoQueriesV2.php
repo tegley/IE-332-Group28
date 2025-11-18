f96d1fdc548fe28f72beeeafeb712ab8af74f727
ekaterinac2195-ux <ekaterinac2195@gmail.com>
2025-11-17 20:58:48 -0500

    Update SCM_homepageBasicCompanyInfoQueriesV2.php
    Added Key Performance Queries

--- supplychainmanager-module/homepage/editing-documents/SCM_homepageBasicCompanyInfoQueriesV2.php
+++ supplychainmanager-module/homepage/editing-documents/SCM_homepageBasicCompanyInfoQueriesV2.php
@@ -159,6 +159,55 @@ $tmp = explode(',', $tmp);
     }
     // echo json_encode($adjustments);
 
+    //Queries for Key Performance
+    $shipmentDetailsQuery = "SELECT AVG(s.ActualDate - s.PromisedDate), STDDEV(s.ActualDate - s.PromisedDate), COUNT(*) FROM Company c JOIN Shipping s ON c.CompanyID = s.SourceCompanyID 
+    WHERE CompanyName = '" . $tmp[0] . "' AND s.ActualDate BETWEEN '" . $tmp[1] . "' AND '" . $tmp[2] . "' AND s.ActualDate <= s.PromisedDate;";
+    // echo $shipmentDetailsQuery;
+     //Execute the SQL query
+    $resultshipmentDetails = mysqli_query($conn, $shipmentDetailsQuery);
+    // Convert the table into individual rows and reformat.
+    $shipmentDetails = []; //Creeating Depended On Array
+    while ($row = mysqli_fetch_array($resultshipmentDetails, MYSQLI_ASSOC)) {
+        $shipmentDetails[] = $row;
+    }
+    // echo json_encode($shipmentDetails);
+
+    $totalShipmentsQuery = "SELECT COUNT(*) FROM Company c JOIN Shipping s ON c.CompanyID = s.SourceCompanyID
+    WHERE CompanyName = '" . $tmp[0] . "' AND s.ActualDate BETWEEN '" . $tmp[1] . "' AND '" . $tmp[2] . "';";
+    // echo $totalShipmentsQuery;
+    //Execute the SQL query
+    $resultstotalShipments = mysqli_query($conn, $totalShipmentsQuery);
+    // Convert the table into individual rows and reformat.
+    $totalShipments = []; //Creeating Depended On Array
+    while ($row = mysqli_fetch_array($resultstotalShipments, MYSQLI_ASSOC)) {
+        $totalShipments[] = $row;
+    }
+    // echo json_encode($totalShipments);
+
+
+    $pastHealthScoresQuery = "SELECT f.HealthScore FROM Company c JOIN FinancialReport f ON c.CompanyID = f.CompanyID WHERE c.CompanyName = '" . $tmp[0] . "' ORDER BY f.RepYear DESC, f.Quarter DESC LIMIT 5;";
+    // echo $pastHealthScoresQuery;
+    //Execute the SQL query
+    $resultspastHealthScores = mysqli_query($conn, $pastHealthScoresQuery);
+    // Convert the table into individual rows and reformat.
+    $pastHealthScores = []; //Creeating Depended On Array
+    while ($row = mysqli_fetch_array($resultspastHealthScores, MYSQLI_ASSOC)) {
+        $pastHealthScores[] = $row;
+    }
+    // echo json_encode($pastHealthScores);
+
+    $disruptionEventsQuery = "SELECT d.EventID, x.CategoryName, d.EventDate, d.EventRecoveryDate, x.Description FROM DisruptionEvent d JOIN DisruptionCategory x ON d.CategoryID = x.CategoryID JOIN ImpactsCompany i ON d.EventID = i.EventID JOIN Company c ON i.AffectedCompanyID = c.CompanyID
+    WHERE c.CompanyName = '" . $tmp[0] . "' GROUP BY x.CategoryName;";
+    // echo $disruptionEvents;
+    //Execute the SQL query
+    $resultsdisruptionEvents = mysqli_query($conn, $disruptionEventsQuery);
+    // Convert the table into individual rows and reformat.
+    $disruptionEvents = []; //Creeating Depended On Array
+    while ($row = mysqli_fetch_array($resultsdisruptionEvents, MYSQLI_ASSOC)) {
+        $disruptionEvents[] = $row;
+    }
+    // echo json_encode($disruptionEvents);
+
     //Making JSON Object
     $SCMHomePageCompanyResults = [
         "companyInfo" => $companyInfo,
@@ -169,7 +218,11 @@ $tmp = explode(',', $tmp);
         "dependsOn" => $dependsOn,
         "shipping" => $shipping,
         "receivings" => $receivings,
-        "adjustments" => $adjustments
+        "adjustments" => $adjustments,
+        "shipmentDetails" => $shipmentDetails,
+        "totalShipments" => $totalShipments,
+        "pastHealthScores" => $pastHealthScores,
+        "disruptionEvents" => $disruptionEvents
     ];
 
     echo json_encode($SCMHomePageCompanyResults);
