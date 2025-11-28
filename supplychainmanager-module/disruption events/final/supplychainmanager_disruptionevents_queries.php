350e9e79fcff45bdea94e00195895d3b39edae22
Timothy Egley <150618476+tegley@users.noreply.github.com>
2025-11-27 21:24:06 -0500

    Update supplychainmanager_disruptionevents_queries.php

--- supplychainmanager-module/disruption events/final/supplychainmanager_disruptionevents_queries.php
+++ supplychainmanager-module/disruption events/final/supplychainmanager_disruptionevents_queries.php
@@ -29,18 +29,20 @@ $filter_option = explode(',', $filter_option);
 //echo "{$user_input[0]}";
 
 //Select statements
-$DF_charts_query = "SELECT c.CompanyName, COUNT(*) As Total";
+$DF_chart_query = "SELECT c.CompanyName, (COUNT(*) / TIMESTAMPDIFF(MONTH, '{$filter_option[1]}', '{$filter_option[2]}')) As Total";
 
-$HDR_charts_query = "SELECT c.CompanyName, ";
+$HDR_chart_query = "SELECT c.CompanyName, SUM(CASE WHEN i.ImpactLevel = 'High' THEN 1 ELSE 0 END) AS NumHighImpact";
 
-$DSD_charts_query = "SELECT c.CompanyName, SUM(CASE WHEN i.ImpactLevel = 'High' THEN 1 ELSE 0 END) AS NumHighImpact, 
+$DSD_chart_query = "SELECT c.CompanyName, SUM(CASE WHEN i.ImpactLevel = 'High' THEN 1 ELSE 0 END) AS NumHighImpact, 
 SUM(CASE WHEN i.ImpactLevel = 'Medium' THEN 1 ELSE 0 END) AS NumMedImpact, 
 SUM(CASE WHEN i.ImpactLevel = 'Low' THEN 1 ELSE 0 END) AS NumLowImpact";
+
+$TD_ART_chart_query = "SELECT DATEDIFF(e.EventRecoveryDate, e.EventDate) AS Downtime";
 
 //Common FROM statement
 $from = "FROM Company c JOIN ImpactsCompany i ON i.AffectedCompanyID = c.CompanyID JOIN DisruptionEvent e ON e.EventID = i.EventID JOIN Location l ON l.LocationID = c.LocationID";
 
-//Common WHERE statement 
+//Common WHERE statement
 $where = "WHERE ((e.EventDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') OR (e.EventRecoveryDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') OR (e.EventDate < '{$filter_option[1]}' AND e.EventRecoveryDate > '{$filter_option[2]}'))";
 
 //Option 1 - Country
@@ -57,12 +59,7 @@ if($filter_option[0] == 'country'){
     //Country GROUP BY and HAVING statements
     $group_by = "GROUP BY c.CompanyName, l.CountryName";
     $having = "HAVING l.CountryName = '{$user_input[0]}'";
-    
-    //Add HDR subquery
-    $HDR_charts_query .= "( SUM(CASE WHEN i.ImpactLevel = 'High' THEN 1 ELSE 0 END) * 1.0 / (SELECT SUM(CASE WHEN i2.ImpactLevel = 'High' THEN 1 ELSE 0 END) 
-    FROM Company c2 JOIN ImpactsCompany i2 ON i2.AffectedCompanyID = c2.CompanyID JOIN DisruptionEvent e2 ON e2.EventID = i2.EventID JOIN Location l2 ON l2.LocationID = c2.LocationID 
-    WHERE ((e2.EventDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') OR (e2.EventRecoveryDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') 
-    OR (e2.EventDate < '{$filter_option[1]}' AND e2.EventRecoveryDate > '{$filter_option[2]}')) AND l2.CountryName = '{$user_input[0]}' ) ) AS HDR";}
+}
 
 //Option 2 - Continent
 if($filter_option[0] == 'continent'){
@@ -75,15 +72,9 @@ if($filter_option[0] == 'continent'){
         $conn->close();
     }
 
-    //Continent HAVING statement
+    //Continent GROUP BY and HAVING statement
     $group_by = "GROUP BY c.CompanyName, l.ContinentName";
     $having = "HAVING l.ContinentName = '{$user_input[0]}'";
-    
-    //Add HDR subquery
-    $HDR_charts_query .= "( SUM(CASE WHEN i.ImpactLevel = 'High' THEN 1 ELSE 0 END) * 1.0 / (SELECT SUM(CASE WHEN i2.ImpactLevel = 'High' THEN 1 ELSE 0 END) 
-    FROM Company c2 JOIN ImpactsCompany i2 ON i2.AffectedCompanyID = c2.CompanyID JOIN DisruptionEvent e2 ON e2.EventID = i2.EventID JOIN Location l2 ON l2.LocationID = c2.LocationID 
-    WHERE ((e2.EventDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') OR (e2.EventRecoveryDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') 
-    OR (e2.EventDate < '{$filter_option[1]}' AND e2.EventRecoveryDate > '{$filter_option[2]}')) AND l2.ContinentName = '{$user_input[0]}' ) ) AS HDR";
 }
 
 //Option 3 - Company
@@ -97,12 +88,9 @@ if($filter_option[0] == 'company'){
         $conn->close();
     }
 
-    //Company HAVING statement
+    //Company GROUP BY and HAVING statements
     $group_by = "GROUP BY c.CompanyName";
     $having = "HAVING c.CompanyName = '{$user_input[0]}'";
-    
-    //Add HDR value calculation
-    $HDR_charts_query .= "SUM(CASE WHEN i.ImpactLevel = 'High' THEN 1 ELSE 0 END) AS HDR";
 }
 
 //Option 4 - Tier
@@ -116,44 +104,72 @@ if($filter_option[0] == 'tier'){
         $conn->close();
     }
 
-    //Tier HAVING statement
+    //Tier GROUP BY and HAVING statements
     $group_by = "GROUP BY c.CompanyName, c.TierLevel";
     $having = "HAVING c.TierLevel = '{$user_input[0]}'";
-    
-    //Add HDR subquery
-    $HDR_charts_query .= "( SUM(CASE WHEN i.ImpactLevel = 'High' THEN 1 ELSE 0 END) * 1.0 / (SELECT SUM(CASE WHEN i2.ImpactLevel = 'High' THEN 1 ELSE 0 END) 
-    FROM Company c2 JOIN ImpactsCompany i2 ON i2.AffectedCompanyID = c2.CompanyID JOIN DisruptionEvent e2 ON e2.EventID = i2.EventID JOIN Location l2 ON l2.LocationID = c2.LocationID 
-    WHERE ((e2.EventDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') OR (e2.EventRecoveryDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') 
-    OR (e2.EventDate < '{$filter_option[1]}' AND e2.EventRecoveryDate > '{$filter_option[2]}')) AND c2.TierLevel = '{$user_input[0]}' ) ) AS HDR";
 }
+
+//HDR subquery version
+/*
+$HDR_charts_query .= "( SUM(CASE WHEN i.ImpactLevel = 'High' THEN 1 ELSE 0 END) * 1.0 / (SELECT SUM(CASE WHEN i2.ImpactLevel = 'High' THEN 1 ELSE 0 END) 
+FROM Company c2 JOIN ImpactsCompany i2 ON i2.AffectedCompanyID = c2.CompanyID JOIN DisruptionEvent e2 ON e2.EventID = i2.EventID JOIN Location l2 ON l2.LocationID = c2.LocationID 
+WHERE ((e2.EventDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') OR (e2.EventRecoveryDate BETWEEN '{$filter_option[1]}' AND '{$filter_option[2]}') 
+OR (e2.EventDate < '{$filter_option[1]}' AND e2.EventRecoveryDate > '{$filter_option[2]}')) AND c2.TierLevel = '{$user_input[0]}' ) ) AS HDR";
+//AND l2.ContinentName = '{$user_input[0]}'
+//AND l2.CountryName = '{$user_input[0]}'
+*/
 
 //Build queries
-$DF_charts_query .= " {$from} {$where} {$group_by} {$having}";
-$DSD_charts_query .= " {$from} {$where} {$group_by} {$having}";
-$HDR_charts_query .= " {$from} {$where} {$group_by} {$having}";
-//echo "\n {$HDR_charts_query} \n";
+$DF_chart_query .= " {$from} {$where} {$group_by} {$having}";
+$DSD_chart_query .= " {$from} {$where} {$group_by} {$having}";
+$HDR_chart_query .= " {$from} {$where} {$group_by} {$having}";
+
+//Modify GROUP BY statement for TD/ART chart query
+switch($filter_option[0]){
+    case "country": 
+        $group_by = "GROUP BY l.CountryName, e.EventID";
+        break;
+    case "continent": 
+        $group_by = "GROUP BY l.ContinentName, e.EventID";
+        break;
+    case "company": 
+        $group_by = "GROUP BY c.CompanyName, e.EventID";
+        break;
+    case "tier": 
+        $group_by = "GROUP BY c.TierLevel, e.EventID";
+        break;
+}
+
+$TD_ART_chart_query .= " {$from} {$where} {$group_by} {$having}";
+//echo "\n {$TD_ART_chart_query} \n";
 
 //Run all queries
-$DF_charts_query_result = mysqli_query($conn, $DF_charts_query);
-while ($row = mysqli_fetch_array($DF_charts_query_result, MYSQLI_ASSOC)) {
-    $DF_charts[] = $row;
+$DF_chart_query_result = mysqli_query($conn, $DF_chart_query);
+while ($row = mysqli_fetch_array($DF_chart_query_result, MYSQLI_ASSOC)) {
+    $DF_chart[] = $row;
 }
 
-$DSD_charts_query_result = mysqli_query($conn, $DSD_charts_query);
-while ($row = mysqli_fetch_array($DSD_charts_query_result, MYSQLI_ASSOC)) {
-    $DSD_charts[] = $row;
+$DSD_chart_query_result = mysqli_query($conn, $DSD_chart_query);
+while ($row = mysqli_fetch_array($DSD_chart_query_result, MYSQLI_ASSOC)) {
+    $DSD_chart[] = $row;
 }
 
-$HDR_charts_query_result = mysqli_query($conn, $HDR_charts_query);
-while ($row = mysqli_fetch_array($HDR_charts_query_result, MYSQLI_ASSOC)) {
-    $HDR_charts[] = $row;
+$HDR_chart_query_result = mysqli_query($conn, $HDR_chart_query);
+while ($row = mysqli_fetch_array($HDR_chart_query_result, MYSQLI_ASSOC)) {
+    $HDR_chart[] = $row;
+}
+
+$TD_ART_query_result = mysqli_query($conn, $TD_ART_chart_query);
+while ($row = mysqli_fetch_array($TD_ART_query_result, MYSQLI_ASSOC)) {
+    $TD_ART_chart[] = $row;
 }
 
 //Create and encode JSON object
 $SCMDisruptionEventResults = [
-    "DF" => $DF_charts,
-    "DSD" => $DSD_charts,
-    "HDR" => $HDR_charts
+    "DF" => $DF_chart,
+    "DSD" => $DSD_chart,
+    "HDR" => $HDR_chart,
+    "TD_ART" => $TD_ART_chart
 ];
 
 echo json_encode($SCMDisruptionEventResults);
