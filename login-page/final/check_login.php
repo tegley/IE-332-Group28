6719be881fb152f1229252cd1cc2ead9fd0009a5
hbavone <hbavo0301@gmail.com>
2025-12-06 17:55:36 -0500

    Update check_login.php

--- login-page/final/check_login.php
+++ login-page/final/check_login.php
@@ -15,7 +15,7 @@ $tmp= array($user_username,$user_password); //[0]=Username,[1]=Password
 $result_array = array(); //Username is 0; Password is 1
 
 //Query - check if username is in the database
-$check_username = "SELECT Username, Password FROM User WHERE Username='" .$tmp[0]. "'";
+$check_username = "SELECT Username, Password FROM User WHERE BINARY Username='" .$tmp[0]. "'";
 $result_check_username = mysqli_query($conn, $check_username);
 $first_result = mysqli_fetch_array($result_check_username, MYSQLI_NUM); //Capture query result
 if ($first_result[0] == NULL) { //Check is username is valid
@@ -24,8 +24,8 @@ if ($first_result[0] == NULL) { //Check is username is valid
     $conn->close();
 }
 else {
-    //Query - check if password is correct
-    $check_password_correct = "SELECT Username, Password FROM User WHERE Username='" .$tmp[0]. "' AND Password='" .$tmp[1]. "'";
+    //Query - check if password is correct BINARY allows case sensitivity checks
+    $check_password_correct = "SELECT Username, Password FROM User WHERE BINARY Username='" .$tmp[0]. "' AND BINARY Password='" .$tmp[1]. "'";
     $result_check_password_correct = mysqli_query($conn, $check_password_correct);
     $second_result = mysqli_fetch_array($result_check_password_correct,MYSQLI_NUM); //Capture query result
     if($second_result[0]==NULL) { //Check if password is correct
@@ -35,7 +35,7 @@ else {
         }
     else {
         //Query - return role from database
-        $sql = "SELECT Role, FullName FROM User WHERE Username='" .$tmp[0]. "'AND Password='" .$tmp[1]. "'";
+        $sql = "SELECT Role, FullName FROM User WHERE BINARY Username='" .$tmp[0]. "'AND BINARY Password='" .$tmp[1]. "'";
         $final_result=mysqli_query($conn, $sql);
         $parsed_result = mysqli_fetch_array($final_result, MYSQLI_NUM);
         $result_array[0] = $parsed_result[0]; //Role
