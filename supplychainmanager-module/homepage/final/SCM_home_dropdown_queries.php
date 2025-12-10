d2daf95c6dcf310f5577791995d46baada935b1b
Timothy Egley <150618476+tegley@users.noreply.github.com>
2025-12-09 22:01:06 -0500

    Update SCM_home_dropdown_queries.php

--- supplychainmanager-module/homepage/final/SCM_home_dropdown_queries.php
+++ supplychainmanager-module/homepage/final/SCM_home_dropdown_queries.php
@@ -11,9 +11,47 @@ if ($conn->connect_error) {
     die("Connection failed:" . $conn->connect_error);
 }
 
-//Always - $tmp[0] = ID, $tmp[1] = Distributors or Transactions
+//Always - $tmp[0] = ID, $tmp[1] = Distributors or Transactions, $tmp[2] = Type (Transactions type), $tmp[3] = CompanyType
 $tmp = $_GET['q'];
 $tmp = explode('|', $tmp);
+
+
+if($tmp[1]=='Transactions') {
+    $transaction_ids_array = [];
+    if($tmp[2] == 'Adjustment'){ //Adjustment
+        $transaction_id_list_query = "SELECT DISTINCT(TransactionID) FROM InventoryAdjustment WHERE CompanyID = {$tmp[0]}";
+    }
+
+    else if($tmp[2] == 'Shipping'){
+        switch($tmp[3]){
+            case "Manufacturer": 
+                $transaction_id_list_query = "SELECT DISTINCT(TransactionID) FROM Shipping WHERE SourceCompanyID = {$tmp[0]}";
+                break;
+            case "Distributor": 
+                $transaction_id_list_query = "SELECT DISTINCT(TransactionID) FROM Shipping WHERE DistributorID = {$tmp[0]}";
+                break;
+            case "Retailer": 
+                echo "Error - retailers don't have shipping transactions";
+                break;
+        }
+    }
+    else if($tmp[2] == 'Receiving'){
+        $transaction_id_list_query = "SELECT DISTINCT(TransactionID) FROM Receiving WHERE ReceiverCompanyID = {$tmp[0]}";
+    }
+    
+    //Run the query
+    $transaction_id_list_result = mysqli_query($conn, $transaction_id_list_query);
+    while ($row = mysqli_fetch_array($transaction_id_list_result, MYSQLI_ASSOC)) {
+        $transaction_ids_array[] = $row;
+    }
+
+    //JSON object conversion
+    $TransactionIDListResults = [
+        "TransactionIDs"=> $transaction_ids_array
+        ];
+
+    echo json_encode($TransactionIDListResults);
+}
 
 
 if($tmp[1]=='Distributor') {
@@ -50,5 +88,3 @@ if($tmp[1]=='Distributor') {
 
 $conn->close();
 ?>
-
-    
